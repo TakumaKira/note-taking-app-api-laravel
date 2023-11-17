e5741425765b95c08ec4ce512450b7763a8183f5
Takuma Kira <takuma@kirakiraworx.com>
2023-11-17 17:54:07 +0900

    [dev] Build and apply NoteController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/notes', [NoteController::class, 'index']);
+Route::get('/notes/{id}', [NoteController::class, 'show']);
+Route::post('/notes', [NoteController::class, 'store']);
+Route::put('/notes/{id}', [NoteController::class, 'update']);
+Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
